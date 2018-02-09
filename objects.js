fcbc7ad87a529ff165428be4e3317a77faf03a79
Dylan L Jones <darkraikiri@gmail.com>
2018-02-09 20:39:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Artist: "song name"}
 function updatePlaylist(playlist, artist, songTitle){
-  return playlist.assign({}, playlist, {[artist]:songTitle})
+  return Object.assign({}, playlist, {[artist]:songTitle})
 }
 function removeFromPlaylist(playlist, artist){
   delete playlist.artist
